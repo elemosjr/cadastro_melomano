5aa24d3665daca49450f0dfe19750d964c6285dd
Eduardo Lemos Júnior <elemosjr@gmail.com>
2021-12-02 18:22:35 -0300

    update requirements + add venv pyinstaller

--- bin/installer.bat
+++ bin/installer.bat
@@ -1,2 +1,5 @@
 cd ..
-pyinstaller -F -n "Cadastro Melomano" --distpath bin/win --workpath bin/win --specpath win --icon=../cadastro_melomano/static/favicon.ico --add-data ../cadastro_melomano;cadastro_melomano app.py
+python -m venv bin/win/venv
+call bin/win/venv/Scripts/activate.bat
+python -m pip install -r requirements.txt
+pyinstaller -n "Cadastro Melomano" --paths bin/win/venv/Lib/site-packages --distpath bin/win/dist --workpath bin/win/build --specpath bin/win/spec --icon=../../../cadastro_melomano/static/favicon.ico --add-data ../../../cadastro_melomano;cadastro_melomano --add-data ../../../.env;. app.py
--- bin/installer.sh
+++ bin/installer.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 cd ..
-pyinstaller -n "Cadastro Melomano" --distpath bin/linux --workpath bin/linux/build --specpath bin/linux/spec --icon="cadastro_melomano/static/favicon.ico" --add-data "../../../cadastro_melomano:cadastro_melomano" app.py
+python -m venv bin/linux/venv
+source bin/linux/venv/bin/activate
+python -m pip install -r requirements.txt
+pyinstaller -n "Cadastro Melomano" --paths bin/linux/venv/lib/python*/site-packages --distpath bin/linux/dist --workpath bin/linux/build --specpath bin/linux/spec --icon="cadastro_melomano/static/favicon.ico" --add-data "../../../cadastro_melomano:cadastro_melomano" --add-data "../../../.env:." app.py
